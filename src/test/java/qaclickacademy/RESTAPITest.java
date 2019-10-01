01a67f4b27cd0f7147991ad20e06a6868b484d73
ThSaK <karthiga81@gmail.com>
2019-10-01 10:30:22 -0700

    First Commit

---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+@Test
+public void PostJIRA()
+{
+	System.out.println("PostJIRA");	
+}
+@Test
+public void DeleteTwitter()
+{
+	System.out.println("DeleteTwitter");
+}
+}
